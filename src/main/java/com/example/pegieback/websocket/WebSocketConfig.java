2133b2be8bbeac00f7c23fbc0c60bbd5e58d587f
Yerlan <isaev.yerlan@kase.kz>
2022-08-07 19:06:17 +0600

    cors

--- src/main/java/com/example/pegieback/websocket/WebSocketConfig.java
+++ src/main/java/com/example/pegieback/websocket/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.example.pegieback.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -16,12 +19,14 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
         config.setApplicationDestinationPrefixes("/app");
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket")
-                .setAllowedOrigins("http://localhost:4200")
-                .setAllowedOrigins("https://effervescent-dusk-cf7920.netlify.app/")
-//                .setAllowedOrigins("*")
-                .withSockJS();
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer () {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
