2499e0e7b6b3b1504f101394a461655ce880db0e
Yerlan <isaev.yerlan@kase.kz>
2022-08-07 19:25:14 +0600

    cors

--- src/main/java/com/example/pegieback/controller/ChatController.java
+++ src/main/java/com/example/pegieback/controller/ChatController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ChatController {
 
     @Autowired
