fe6bbd351e6aa522e192a697e26c0ff6813819ea
Yerlan <isaev.yerlan@kase.kz>
2022-08-07 19:14:37 +0600

    cors

--- src/main/java/com/example/pegieback/CorsFilter.java
+++ src/main/java/com/example/pegieback/CorsFilter.java
@@ -1,3 +1,14 @@
+package com.example.pegieback;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
